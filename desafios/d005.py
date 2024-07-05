b985c3ca508ee821374f3883c7ae838497b5c3be
vambergestevao <vambergfreitas@gmail.com>
2024-07-05 12:47:56 -0300

    Create d005 e d006

---
+++ desafios/d005.py
@@ -0,0 +1,10 @@
+print('====== DESAFIO 05 ======')
+
+n = int(input('Digite um número: '))
+
+# a = n - 1
+# s = n + 1
+
+# print('Analise o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
+
+print('Analise o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
---
+++ desafios/d006.py
@@ -0,0 +1,17 @@
+print('====== DESAFIO 06 ======')
+
+n = int(input('Digite um número: '))
+
+# d = n * 2
+# t = n * 3
+# r = n ** (1/2)
+
+# print('O dobro de {} vale {}'.format(n, d))
+# print('O triplo de {} vale {}'.format(n, t))
+# print('A raiz quadrada de {} é igual a {:.2f}'.format(n, r))
+
+# A função 'pow(n, (1/2))', calcula a raiz quadrada.  
+
+print('O dobro de {} vale {}'.format(n, (n*2)))
+print('O triplo de {} vale {}'.format(n, (n*3)))
+print('A raiz quadrada de {} é igual a {:.2f}'.format(n, (n**(1/2))))
