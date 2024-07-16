03c19a48fc3f1ec91c88da789c1b0e5b7d05b12f
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-07-16 15:51:57 -0300

    Create ex004 and ex005

---
+++ exercicios/ex004.py
@@ -0,0 +1,12 @@
+print('====== EXERCÍCIO 04 ======')
+
+import math
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print('A raiz de {} é igual a {:.2f}'.format(n, raiz))
+
+
+from math import sqrt, floor
+n = int(input('Digite um número: '))
+raiz = sqrt(n)
+print('A raiz de {} é igual a {:.2f}'.format(n, floor(raiz)))
