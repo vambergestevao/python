50b275263bf8440a4640a4441dad8d030619c929
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-07-17 17:43:57 -0300

    Create d016

---
+++ desafios/d016.py
@@ -0,0 +1,15 @@
+print('====== DESAFIO 16 ======')
+
+# from math import trunc
+# n = float(input('Digite um valor: '))
+# p = trunc(n)
+# print('O valor digitado foi {} e a porção inteira é {}'.format(n, p))
+
+
+from math import trunc
+n = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a porção inteira é {}'.format(n, trunc(n)))
+
+
+# n = float(input('Digite um valor: '))
+# print('O valor digitado foi {} e a porção inteira é {}'.format(n, int(n)))
