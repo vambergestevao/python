b82ea524518e8a9df9de99757268ce12d0fef582
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-08-30 18:35:11 -0300

    Create d024

---
+++ desafios/d024.py
@@ -0,0 +1,7 @@
+print('====== DESAFIO 24 ======')
+
+city = str(input('Em que cidade você nasceu? ')).strip().title()
+print('Santo' in city)
+
+# city = str(input('Em que cidade você nasceu? ')).strip()
+# print(city[:5].upper() == 'SANTO')
