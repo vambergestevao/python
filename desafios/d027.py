0f52211e64d161f68f452e15c9eea9731b207c9f
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-09-09 18:02:34 -0300

    Create d027

---
+++ desafios/d027.py
@@ -0,0 +1,7 @@
+print('====== DESAFIO 27 ======')
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Muito prazer em te conhecer!')
+n = nome.split()
+print('Seu primeiro nome é {}'.format(n[0]))
+print('Seu último nome é {}'.format(n[len(n)-1]))
