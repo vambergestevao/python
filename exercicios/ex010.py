9dfed0cae028f7299dc1aa137c2f8d6643976933
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-10-16 22:29:30 -0300

    Create ex010

---
+++ exercicios/ex010.py
@@ -0,0 +1,55 @@
+print('\033[1;37m====== EXERCÍCIO 10 ======\033[m')
+
+print('\033[1;33m- Estrutura de Repetição FOR -\033[m')
+
+for c in range(0, 6):
+    print('Oi')
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+for c in range(0, 6): # Sempre desconsidera o último número
+    print(c)
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+for c in range(6, 0, -1): # (-1) Contagem inversa
+    print(c)
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+for c in range(0, 7, 2): # (2) Conta de 2 em 2
+    print(c)
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+print('Fim!')
+
+print('\033[1;31m=-\033[m' * 15)
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n # s = s + n
+print('O somatório de todos os valores foi {}'.format(s))
