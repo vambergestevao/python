a1f0e0b787e4c6508ae26c49439a524c5c0e5ccf
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-10-22 16:46:31 -0300

    Create d047

---
+++ desafios/d047.py
@@ -0,0 +1,7 @@
+print('\033[1;37m====== DESAFIO 47 ======\033[m')
+
+print('\033[1;33m- Estrutura de Repetição FOR -\033[m')
+
+for c in range(2, 51, 2): # (2) Conta de 2 em 2
+    print(c, end=' ') # (end=' ') Cria a sequência na Horizontal
+print('Acabou!')
