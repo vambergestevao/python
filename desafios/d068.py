a7e09b41834e05651170a6d2d79abf33cd932a0d
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-11-27 13:32:29 -0300

    Create d068

--- desafios/d068.py
+++ desafios/d068.py
@@ -2,4 +2,30 @@ print('\033[1;37m====== DESAFIO 68 ======\033[m')
 
 print('\033[1;33m- Estrutura de Repetição WHILE -\033[m')
 
-from 
+from random import randint
+v = 0
+while True:
+    player = int(input('Diga um valor: '))
+    pc = randint(0, 10)
+    total = player + pc
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {player} e o computador {pc}. Total de {total}', end='')
+    print(' DEU PAR' if total % 2 == 0 else ' DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('VOCÊ VENCEU!')
+            v += 1
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('VOCÊ VENCEU!')
+            v += 1
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Você venceu {v} vezes.')
