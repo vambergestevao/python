06898e82214df87ad4614895071026f5c07eadcc
Vamberg Estevão <165962067+vambergestevao@users.noreply.github.com>
2024-12-12 16:18:26 -0300

    Update d071

--- desafios/d071.py
+++ desafios/d071.py
@@ -1,7 +1,29 @@
-print('\033[1;37m====== DESAFIO 70 ======\033[m')
+print('\033[1;37m====== DESAFIO 71 ======\033[m')
 
 print('\033[1;33m- Estrutura de Repetição WHILE -\033[m')
 
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+céd = 50
+totcéd = 0
 while True:
-    valorsac = int(input('Quanto quer sacar: '))
-    
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
